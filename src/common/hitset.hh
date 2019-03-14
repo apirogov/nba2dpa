3ede1144309e145b4e0f5f64dc778f4c9eb32191
Anton Pirogov <apirogov@users.noreply.github.com>
2019-03-14 14:20:53 +0100

    added greedy hitting set function

---
+++ src/common/hitset.hh
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+
+//input: list of sets. output: a hitting set
+//(i.e., set containing at least one element from each set in the list)
+std::set<int> greedy_hitting_set(std::map<int,std::set<int>> const& s2u) {
+  // auto s2u = sets; //copy, as we will modify it
+  // now we need the reverse mapping from "universe" to sets
+  // (we conceptually have a bipartite graph)
+
+  //map elements of universe to sets they appear in
+  std::map<int,std::set<int>> u2s;
+  for (auto const& s : s2u) {
+    for (auto const e : s.second) {
+      u2s[e].emplace(s.first);
+    }
+  }
+
+  std::set<int> h; //hitting set
+
+  //each loop iteration starts with remaining uncovered sets on the left as reachable sets from right
+  //and useful hitting set candidates on the right which reach at least one uncovered set on left
+  //after an iteration, covered sets are unreachable and isolated elements on the right removed.
+  //invariant: el has edge to set -> set has edge to el, and every remaining el has an edge to some set
+  while (!u2s.empty()) {
+    //sort candidates by how many yet uncovered sets they would cover
+    std::vector<int> cands;
+    for (auto const& it : u2s)
+      cands.push_back(it.first);
+
+    std::sort(std::begin(cands), std::end(cands), [&u2s](int a, int b){
+        auto const sza = u2s.at(a).size();
+        auto const szb = u2s.at(b).size();
+        //sort desc. by # of touched sets, then asc. by val
+        return  sza > szb || (sza == szb && a < b);
+    });
+
+    //take greedily a candidate that covers as many uncovered sets as possible
+    auto const newel = cands.front();
+    h.emplace(newel); //add it to hitting set
+    auto const curcovered = u2s.at(newel); //get newly covered sets
+
+    // cerr << "picked " << newel << " freshly covering " << seq_to_str(curcovered) << endl;
+
+    //make each covered set remove itself from all its elements, i.e. kill incoming edges
+    //this makes those sets unreachable from right side
+    for (auto const sidx : curcovered) {
+      for (auto const el : s2u.at(sidx)) {
+        // the set sidx was uncovered before, i.e. all its elements are still candidates
+        // hence there are no el in s2u[sidx] that are not in u2s anymore
+        // those candidates must also still have an edge back to set sidx
+        // hence no need to check for null/end
+        u2s.at(el).erase(u2s.at(el).find(sidx));
+      }
+    }
+
+    //conceptually, we could remove covered sets
+    //practically, it suffices that they are unreachable from the elements now
+    /*
+    for (auto const sidx : curcovered) {
+      auto it = s2u.find(sidx);
+      if (it != end(s2u))
+        s2u.erase(it);
+    }
+    */
+
+    //remove the now useless elements (not in any remaining uncovered set,
+    //i.e. isolated nodes without outgoing edges),
+    //including the new hitting set element, from graph
+    auto it = std::begin(u2s);
+    while (it != std::end(u2s)) {
+      auto const tmp = it++;
+      if (tmp->second.empty()) {
+        // cerr << "remove now useless " << it->first << endl;
+        u2s.erase(tmp);
+      }
+    }
+  }
+
+  return h;
+}
+
--- src/tools/scratchpad.cc
+++ src/tools/scratchpad.cc
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <algorithm>
 #include <random>
-#include <bitset>
+#include <map>
 #include <set>
 #include <vector>
 #include <map>
@@ -8,6 +9,7 @@
 
 #include "common/types.hh"
 #include "common/trie_map.hh"
+#include "common/hitset.hh"
 #include "aut.hh"
 #include "io.hh"
 // #include "graph.hh"
@@ -22,26 +24,45 @@
 using namespace std;
 using namespace nbautils;
 
-nba_bitset vec_to_bitset(vector<int> const& v) { return to_bitset<nba_bitset>(v); }
-
 //this is for trying stuff out. do whatever you want here.
 int main(int argc, char *argv[]) {
   (void)argv[0];
   (void)argc;
   auto const log = spd::stderr_logger_mt("log");
 
-  auto auts = nbautils::AutStream<Aut<string>>("");
-  while (auts.has_next()) {
-    auto aut = auts.parse_next();
+  greedy_hitting_set({});
 
-    if (!aut.is_sba()) {
-      cout << "Not Büchi!" << endl;
-      return 1;
-    }
+  auto test1 = map<int,set<int>>{{0,{0,1,2}},{1,{0,3,4}},{2,{3,5}}};
+  auto test2 = map<int,set<int>>{{0,{1,2,5}},{1,{2,3,4}},{2,{1,3}}};
+  auto test3 = map<int,set<int>>{ {0,{1,2,3}}, {1,{3,4,5}}, {2,{5,6,7}}, {3,{8,9,10}}, {4,{10,11,12}} };
 
-    ba_trim(aut, log);
-    aut.normalize();
-    print_aut(aut);
+  for (auto const s : test1) {
+    cout << s.first << " -> " << seq_to_str(s.second) << endl;
+  }
+  cout << seq_to_str(greedy_hitting_set(test1)) << endl;
+  cout << "----" << endl;
+  for (auto const s : test2) {
+    cout << s.first << " -> " << seq_to_str(s.second) << endl;
+  }
+  cout << seq_to_str(greedy_hitting_set(test2)) << endl;
+  cout << "----" << endl;
+  for (auto const s : test3) {
+    cout << s.first << " -> " << seq_to_str(s.second) << endl;
+  }
+  cout << seq_to_str(greedy_hitting_set(test3)) << endl;
+
+  // auto auts = nbautils::AutStream<Aut<string>>("");
+  // while (auts.has_next()) {
+  //   auto aut = auts.parse_next();
+
+  //   if (!aut.is_sba()) {
+  //     cout << "Not Büchi!" << endl;
+  //     return 1;
+  //   }
+
+  //   ba_trim(aut, log);
+  //   aut.normalize();
+  //   print_aut(aut);
 
     //ensure its a TDPA
     // aut.make_colored();
@@ -62,6 +83,6 @@ int main(int argc, char *argv[]) {
     // cout << pa_is_empty(aut) << endl;
     // auto run = get_acc_pa_run(aut);
     // cout << seq_to_str(run.first) << " | " << seq_to_str(run.second) << endl;
-  }
+  // }
 
 }
